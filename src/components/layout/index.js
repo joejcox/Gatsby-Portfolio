9854ad4d0b6b6fdbcb877ee5f851737fae115bab
Joe Cox <joecox@live.co.uk>
2021-02-04 01:48:09 +0000

    replaced fontawesome with react icons

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -10,12 +10,6 @@ const Layout = ({ children, title }) => {
     <div className="wrapper">
       <Helmet>
         <title>{title}</title>
-        <link
-          href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
-          rel="stylesheet"
-          integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN"
-          crossorigin="anonymous"
-        />
       </Helmet>
       <div className="wrapper-top">
         <Header />
--- src/components/social-links/index.js
+++ src/components/social-links/index.js
@@ -1,18 +1,20 @@
 import React from "react"
 import "./styles.scss"
+import { ImGithub } from "react-icons/im"
+import { FiInstagram } from "react-icons/fi"
 
 const SocialLinks = () => {
   return (
     <div className="social-links">
       <a href="https://www.github.com/joejcox" rel="noreferrer" target="_blank">
-        <i className="fa fa-github"></i>
+        <ImGithub />
       </a>
       <a
         href="https://www.instagram.com/joecoxo"
         rel="noreferrer"
         target="_blank"
       >
-        <i className="fa fa-instagram"></i>
+        <FiInstagram />
       </a>
     </div>
   )
